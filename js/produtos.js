29613303d849bb8b37417eab3e48c1e613339465
Gabriel BS <paulogabriel.bsm@gmail.com>
2023-04-13 16:34:17 -0300

    produtos estruturado

--- js/produtos.js
+++ js/produtos.js
@@ -1,20 +1,25 @@
-const setaDrop1 = document.querySelector(".triangulo1");
-const setaDrop2 = document.querySelector(".triangulo2");
-const setaDrop3 = document.querySelector(".triangulo3");
-const setaDrop4 = document.querySelector(".triangulo4");
+// const setaDrop1 = document.querySelector(".triangulo1");
+// const setaDrop2 = document.querySelector(".triangulo2");
+// const setaDrop3 = document.querySelector(".triangulo3");
+// const setaDrop4 = document.querySelector(".triangulo4");
 
-setaDrop1.addEventListener("click", () => {
-  setaDrop1.classList.toggle("active");
+const triangulo1 = document.getElementById("tri1");
+const triangulo2 = document.getElementById("tri2");
+const triangulo3 = document.getElementById("tri3");
+const triangulo4 = document.getElementById("tri4");
+
+triangulo1.addEventListener("click", () => {
+  triangulo1.classList.toggle("active");
 });
 
-setaDrop2.addEventListener("click", () => {
-  setaDrop2.classList.toggle("active");
+triangulo2.addEventListener("click", () => {
+  triangulo2.classList.toggle("active");
 });
 
-setaDrop3.addEventListener("click", () => {
-  setaDrop3.classList.toggle("active");
+triangulo3.addEventListener("click", () => {
+  triangulo3.classList.toggle("active");
 });
 
-setaDrop4.addEventListener("click", () => {
-  setaDrop4.classList.toggle("active");
+triangulo4.addEventListener("click", () => {
+  triangulo4.classList.toggle("active");
 });
