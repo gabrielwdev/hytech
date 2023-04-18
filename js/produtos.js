c7683c8117de27af938ba25de3da42a18781e000
Gabriel BS <paulogabriel.bsm@gmail.com>
2023-04-18 16:37:23 -0300

    drop mobile

--- js/produtos.js
+++ js/produtos.js
@@ -8,6 +8,11 @@ const triangulo2 = document.getElementById("tri2");
 const triangulo3 = document.getElementById("tri3");
 const triangulo4 = document.getElementById("tri4");
 
+const triangulo5 = document.getElementById("tri5");
+const triangulo6 = document.getElementById("tri6");
+const triangulo7 = document.getElementById("tri7");
+const triangulo8 = document.getElementById("tri8");
+
 triangulo1.addEventListener("click", () => {
   triangulo1.classList.toggle("active");
 });
@@ -22,4 +27,30 @@ triangulo3.addEventListener("click", () => {
 
 triangulo4.addEventListener("click", () => {
   triangulo4.classList.toggle("active");
+});
+
+const btnFiltro = document.getElementById("btnFiltro");
+
+btnFiltro.addEventListener("click", () => {
+  btnFiltro.classList.toggle("active");
+});
+
+triangulo5.addEventListener("click", () => {
+  triangulo5.classList.toggle("active");
+  console.log("chack");
+});
+
+triangulo6.addEventListener("click", () => {
+  triangulo6.classList.toggle("active");
+  console.log("chack");
+});
+
+triangulo7.addEventListener("click", () => {
+  triangulo7.classList.toggle("active");
+  console.log("chack");
+});
+
+triangulo8.addEventListener("click", () => {
+  triangulo8.classList.toggle("active");
+  console.log("chack");
 });
